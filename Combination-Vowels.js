ece3653411a87f7c621c8163ef0b427ee3970f3e
SYED ABUTHAHIR A <62162295+Abu19CS@users.noreply.github.com>
2021-02-21 19:22:00 +0530

    Question Update

--- Combination-Vowels.js
+++ Combination-Vowels.js
@@ -1,3 +1,63 @@
+// Group By - Combinations of vowels
+// The program must accept N string values as the input. The program must group the string values based on the combination of vowels (No vowels,
+// a, ae, aei, aeio, aeiou, aeiu, aeo, aeou, aeu, ai, aio, aiou, aiu, ao, aou, au, e, ei, eio, eiou, eiu, eo, eou, eu, i, io, iou, iu, o, ou and
+// u). The program must print all possible combinations of the vowels followed by the string values that belong to the cobination as shown in 
+// the Example Input/Output section. The combinations of vowels must be printed in the above mentioned order. The string values that belong to a 
+// combination must be printed in the order of their occurence.
+
+// Boundary Conditions:
+// 1 <= N <= 100
+// 1 <= Length of each string value <= 100
+
+// Input Format:
+// The first line Contain N.
+// The next line contain N string values.
+
+// Output Format:
+// The lines containing the all possible combinations of the vowels followed by the string values that belong to the combinations.
+
+/**
+Example Input/Output 1:
+
+Input:
+12
+rat
+tennis
+normal
+eagle
+winter
+bottle
+intel
+atom
+journey
+escape
+by
+your
+
+Output:
+
+No Vowels - by
+a - rat
+ae - eagle,escape
+ao - normal,atom
+ei - tennis,winter,intel
+eo - bottle
+eou - journey
+
+Example Input/Output 2:
+
+Input:
+3
+GAME
+Mask
+TASK
+
+Output:
+
+a - Mask,TASK
+ae - GAME
+**/
+
 const readline = require('readline');
 const reader = readline.createInterface ({
     input: process.stdin,
